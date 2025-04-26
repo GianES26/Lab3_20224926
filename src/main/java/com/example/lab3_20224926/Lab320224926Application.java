5964a0622498a6d368be17f1a84454bac0f16b18
GianES26 <a20224926@pucp.edu.pe>
2025-04-25 20:34:24 -0500

    Creacion main branch

---
+++ src/main/java/com/example/lab3_20224926/Lab320224926Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20224926;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320224926Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320224926Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab3_20224926/Lab320224926ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20224926;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320224926ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
